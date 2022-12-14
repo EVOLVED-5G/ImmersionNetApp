4e27489dc48801f0f7b6dd39c730bea368a3b8a8
Charles Bailly <charles.bailly@immersion.fr>
2022-12-14 16:00:03 +0100

    Adding running command for the netapp in Dockerfile

--- src/prepare_deploy.sh
+++ src/prepare_deploy.sh
@@ -5,3 +5,4 @@ jq -r .capif_https_port=\"$CAPIF_PORT_HTTPS\" capif_registration.json >> tmp.jso
 jq -r .capif_callback_url=\"http://$CALLBACK_ADDRESS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 evolved5g register-and-onboard-to-capif --config_file_full_path="/usr/src/app/capif_registration.json"
 tail -f /dev/null
+python main.py
