6bce0eecc79c00e12bd84ff2802118e727476568
Charles Bailly <charles.bailly@immersion.fr>
2022-12-14 14:45:20 +0100

    Using Unix-like eol style

--- src/prepare_deploy.sh
+++ src/prepare_deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 jq -r .capif_host=\"$CAPIF_HOSTNAME\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 jq -r .capif_http_port=\"$CAPIF_PORT_HTTP\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 jq -r .capif_https_port=\"$CAPIF_PORT_HTTPS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 jq -r .capif_callback_url=\"http://$CALLBACK_ADDRESS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
-
 evolved5g register-and-onboard-to-capif --config_file_full_path="/usr/src/app/capif_registration.json"
-
 tail -f /dev/null
