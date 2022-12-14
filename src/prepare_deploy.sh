07d252e7fb05531bbfbf9752f086a22858423891
Charles Bailly <charles.bailly@immersion.fr>
2022-12-14 17:00:03 +0100

    Starting automatically the netapp after registration

--- src/prepare_deploy.sh
+++ src/prepare_deploy.sh
@@ -4,5 +4,4 @@ jq -r .capif_http_port=\"$CAPIF_PORT_HTTP\" capif_registration.json >> tmp.json 
 jq -r .capif_https_port=\"$CAPIF_PORT_HTTPS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 jq -r .capif_callback_url=\"http://$CALLBACK_ADDRESS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 evolved5g register-and-onboard-to-capif --config_file_full_path="/usr/src/app/capif_registration.json"
-tail -f /dev/null
 python main.py
